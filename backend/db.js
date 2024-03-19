10fd77eff296e8b2832d8bd807364fbc41340cf4
Aryan9rana <ranaryan9999@gmail.com>
2024-03-19 21:38:44 +0530

    added connection string

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,6 @@
 const mongoose= require('mongoose');
-const mongoURI= "mongodb://127.0.0.1:27017/cloud-notepad";
+// const mongoURI= "mongodb://127.0.0.1:27017/cloud-notepad";
+const mongoURI= "mongodb+srv://uselessemailaryan:oJ5iL5QJkeyUSqyz@cloud-notepad.le9rqva.mongodb.net/?retryWrites=true&w=majority&appName=Cloud-notepad";
 
 const connectToMongo=()=>{
     mongoose.connect(mongoURI)
